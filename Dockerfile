712c7fcad15d6b5ff945dbf01e5a180921639fb9
Reinald Santos <reinaldsantos312@gmail.com>
2026-01-28 16:29:14 +0000

    ULTIMA CORREÇÃO: database.js correto

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,7 @@
 ﻿FROM node:14-alpine
-
 WORKDIR /app
-
 COPY package*.json ./
 RUN npm install --production --legacy-peer-deps
-
 COPY . .
-
 EXPOSE 1337
-
 CMD ["npm", "start"]
--- config/database.js
+++ config/database.js
@@ -1,24 +1,20 @@
-﻿// config/database.js
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL || '');
-
-module.exports = ({ env }) => ({
+﻿module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: config.host || 'dpg-d5kgas94tr6s73au58pg-a.oregon-postgres.render.com',
-        port: config.port || 5432,
-        database: config.database || 'strapi_5cj5',
-        username: config.user || 'meu_admin',
-        password: config.password || 'QUm94o4oPL9x6bhGCX4B4zIGxwZeRRIF',
-        ssl: { rejectUnauthorized: false },
+        host: env('DATABASE_HOST', 'dpg-d5kgas94tr6s73au58pg-a.oregon-postgres.render.com'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi_5cj5'),
+        username: env('DATABASE_USERNAME', 'meu_admin'),
+        password: env('DATABASE_PASSWORD', 'QUm94o4oPL9x6bhGCX4B4zIGxwZeRRIF'),
+        ssl: env.bool('DATABASE_SSL', true)
       },
       options: {
-        ssl: true,
-      },
-    },
-  },
+        ssl: env.bool('DATABASE_SSL', true)
+      }
+    }
+  }
 });
