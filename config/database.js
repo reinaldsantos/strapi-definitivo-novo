ee91864c4faa3c47abca6d1e4bb9066071d69900
Reinald Santos <reinaldsantos312@gmail.com>
2026-01-28 16:21:17 +0000

    FIX: database config strapi 3

--- config/database.js
+++ config/database.js
@@ -1,10 +1,24 @@
-﻿module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: { rejectUnauthorized: false }
+﻿// config/database.js
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL || '');
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: config.host || 'dpg-d5kgas94tr6s73au58pg-a.oregon-postgres.render.com',
+        port: config.port || 5432,
+        database: config.database || 'strapi_5cj5',
+        username: config.user || 'meu_admin',
+        password: config.password || 'QUm94o4oPL9x6bhGCX4B4zIGxwZeRRIF',
+        ssl: { rejectUnauthorized: false },
+      },
+      options: {
+        ssl: true,
+      },
     },
-    pool: { min: 0, max: 5 }
   },
 });
