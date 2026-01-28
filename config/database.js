9b7f154ad13f7b7aba9850a45174019e6f7dd9c7
Reinald Santos <reinaldsantos312@gmail.com>
2026-01-28 17:13:09 +0000

    FIX: database.js correto para strapi 3

--- config/database.js
+++ config/database.js
@@ -1,13 +1,18 @@
 ﻿module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', 'dpg-d5kgas94tr6s73au58pg-a.oregon-postgres.render.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi_5cj5'),
-      user: env('DATABASE_USERNAME', 'meu_admin'),
-      password: env('DATABASE_PASSWORD', 'QUm94o4oPL9x6bhGCX4B4zIGxwZeRRIF'),
-      ssl: env.bool('DATABASE_SSL', true),
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {},
     },
   },
 });
